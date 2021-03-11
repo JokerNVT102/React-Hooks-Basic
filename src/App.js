f70df094d9adcc28417a2393891a2c881fac8300
18211TT3327 <thainguyenv2018@@gmail.com>
2021-03-11 14:58:14 +0700

    Memoization and React.memo

--- src/App.js
+++ src/App.js
@@ -1,130 +1,17 @@
 // import { useEffect, useState } from "react";
 import { useState } from "react";
 import "./App.scss";
-import BetterClock from "./components/BetterClock";
-// import queryString from "query-string";
-// import Pagination from "./components/Pagination";
-// import ColorBox from "./components/ColorBox";
-// import TodoForm from "./components/TodoForm";
-// import TodoList from "./components/TodoList";
-// import PostList from "./components/PostList";
-// import Search from "./components/Search";
-import Clock from "./components/Clock";
-import MagicBox from "./components/MagicBox";
+import Hero from "./components/Hero";
 
 function App() {
-  // const [todoList, setTodoList] = useState([
-  //   { id: 1, title: "I love Easy Frontend! 😍 " },
-  //   { id: 2, title: "We love Easy Frontend! 🥰 " },
-  //   { id: 3, title: "They love Easy Frontend! 🚀 " },
-  // ]);
-
-  // function handleTodoClick(todo) {
-  //   console.log(todo);
-  //   const index = todoList.findIndex((x) => x.id === todo.id);
-  //   if (index < 0) return;
-  //   const newTodoList = [...todoList];
-  //   newTodoList.splice(index, 1);
-  //   setTodoList(newTodoList);
-  // }
-
-  // function handleTodoFormSubmit(formValues) {
-  //   console.log("ok", formValues);
-
-  //   const newTodo = {
-  //     id: todoList.length + 1,
-  //     ...formValues,
-  //   };
-  //   const newTodoList = [...todoList];
-  //   newTodoList.push(newTodo);
-  //   setTodoList(newTodoList);
-  // }
-  // const [postList, setPostList] = useState([]);
-  // const [pagination, setPagination] = useState({
-  //   _page: 1,
-  //   _limit: 10,
-  //   _totalRows: 11,
-  // });
-
-  // const [filters, setFilters] = useState({
-  //   _limit: 10,
-  //   _page: 1,
-  // });
-  //empty lay du lieu 1 lan
-  // useEffect(() => {
-  //   async function fetchPostList() {
-  //     // side effect lay du lieu
-
-  //     try {
-  //       const requestUrl =
-  //         "http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1";
-  //       const response = await fetch(requestUrl);
-  //       const responseJSON = await response.json();
-  //       const { data } = responseJSON;
-  //       setPostList(data);
-  //     } catch (error) {
-  //       console.log("Failed to fetch post list: ", error.message);
-  //     }
-  //   }
-  //   fetchPostList();
-  // }, []);
-  //
-  // useEffect(() => {
-  //   async function fetchPostList() {
-  //     // side effect lay du lieu
-
-  //     try {
-  //       const paramsString = queryString.stringify(filters);
-  //       const requestUrl = `http://js-post-api.herokuapp.com/api/posts?${paramsString}`;
-  //       const response = await fetch(requestUrl);
-  //       const responseJSON = await response.json();
-  //       const { data, pagination } = responseJSON;
-  //       setPagination(pagination);
-  //       setPostList(data);
-  //     } catch (error) {
-  //       console.log("Failed to fetch post list: ", error.message);
-  //     }
-  //   }
-  //   fetchPostList();
-  // }, [filters]);
-  // function handlePageChange(newPage) {
-  //   console.log("page: ", newPage);
-  //   setFilters({
-  //     ...filters,
-  //     _page: newPage,
-  //   });
-  // }
-  // function handleFiltersChange(newFilters) {
-  //   console.log("ok: ", newFilters);
-  //   setFilters({
-  //     ...filters,
-  //     _page: 1,
-  //     title_like: newFilters.searchTerm,
-  //   });
-  // }
-  const [showClock, setShowClock] = useState(true);
+  const [count, setCount] = useState(0);
+  const handleHeroClick = () => {};
   return (
     <div className="app">
       <h1>Welcome to Clock</h1>
-      {/* {showClock && <Clock />}
-      <BetterClock />
-      <button
-        onClick={() => {
-          setShowClock(false);
-        }}
-      >
-        hide clock
-      </button> */}
-      <MagicBox/>
-      {/* <ColorBox/> */}
-      {/* <TodoForm onSubmit = {handleTodoFormSubmit}/>
-     <TodoList 
-     todos ={todoList}
-     onTodoClick ={handleTodoClick}
-     /> */}
-      {/* <Search onSubmit={handleFiltersChange} />
-      <PostList posts={postList} />
-      <Pagination pagination={pagination} onPageChange={handlePageChange} /> */}
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <Hero name="Easy Frontend" onClick={handleHeroClick} />
     </div>
   );
 }
